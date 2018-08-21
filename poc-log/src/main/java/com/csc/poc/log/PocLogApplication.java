b352d6c448b15fa4cc12be56524867f6059c79b5
qiumingbao <qiumingbao@gmail.com>
2018-08-21 16:00:23 +0800

    poc-log integrate log4j2.

--- poc-log/src/main/java/com/csc/poc/log/PocLogApplication.java
+++ poc-log/src/main/java/com/csc/poc/log/PocLogApplication.java
@@ -1,5 +1,7 @@
 package com.csc.poc.log;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class PocLogApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(PocLogApplication.class);
 
     public static void main(String[] args) {
         
         SpringApplication.run(PocLogApplication.class, args);
         
-        System.out.print(PocLogApplication.class.getName() + " started!");
+        LOG.info(PocLogApplication.class.getName() + " started!");
     }
     
     @RequestMapping(value = "/")
     public String index() {
-        return "Hello from Logging POC!";
+        String msg = "Hello from Logging POC!";
+        LOG.info("URL: {}, MSG: {}", "/", msg);
+        return msg;
     }
 }
